46c3edbb2802f28b7cf684f8cfa5ad33f25155dd
milekajtez <milekajtez@gmail.com>
2020-08-13 13:40:48 +0200

    ssd chages

---
+++ AppBackend/AvioCarBackend/AvioCarBackend/Migrations/20200813113406_MigrationSSD.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AvioCarBackend.Migrations
+{
+    public partial class MigrationSSD : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
